24f9a88e6cd38b8f3ebb26110068b4d8ad8eba2a
Cady Baltz <55221077+cadybaltz@users.noreply.github.com>
2020-09-20 13:26:40 -0500

    Updated calendar

--- view/src/pages/calendar.js
+++ view/src/pages/calendar.js
@@ -32,8 +32,9 @@ class calendar extends Component {
           <p className="heading">Calendar</p>
         </div>
         <Container width="80%" className="movingItem">
-            <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%235a5377&amp;ctz=America%2FChicago&amp;src=c3dldXRkQGdtYWlsLmNvbQ&amp;src=NjlnbmszOThiZnUyNGZhc3NrNTQ2cWdtYzhAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;src=ODU1NTZvN2Q1YnZrdmlsZXNuNjU5bWVra2NAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;src=cW80cm1zZ2tzYzZwZTNiZmE5c3NvNGY0MnNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;src=c2phOXJiYmxjMGgzdG9jM2xkYmRudXFibjhAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%238E24AA&amp;color=%23039BE5&amp;color=%239E69AF&amp;color=%233F51B5&amp;color=%23D81B60&amp;showTabs=0&amp;showPrint=0&amp;showTitle=0&amp;showDate=0" style="border:solid 1px #777" style={{border: "0", width:"100%", height:"100vh", frameborder:"0", scrolling:"no"}}></iframe>
+		<iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%235a5377&amp;ctz=America%2FChicago&amp;src=NjlnbmszOThiZnUyNGZhc3NrNTQ2cWdtYzhAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;src=ODU1NTZvN2Q1YnZrdmlsZXNuNjU5bWVra2NAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;src=cW80cm1zZ2tzYzZwZTNiZmE5c3NvNGY0MnNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;src=MHZqMXBqM243ODNxdXNhbDIzaTZyNWRrMDBAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;src=c2phOXJiYmxjMGgzdG9jM2xkYmRudXFibjhAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23039BE5&amp;color=%239E69AF&amp;color=%233F51B5&amp;color=%23B39DDB&amp;color=%23D81B60&amp;showTitle=0&amp;showTabs=0&amp;showPrint=0&amp;showDate=1&amp;showCalendars=1&amp;showTz=1" style="border:solid 1px #777" style={{border: "0", width:"100%", height:"100vh", frameborder:"0", scrolling:"no"}}></iframe>
         </Container>
+	
 			</div>
 		);
 	}
