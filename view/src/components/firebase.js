3d6de8e913b2e84022795a8634335c4436234a21
Reshmi Ranjith <rreshmiranjith@gmail.com>
2020-10-19 17:59:28 -0500

    adjust env vars

--- view/src/components/firebase.js
+++ view/src/components/firebase.js
@@ -2,14 +2,14 @@ const firebase = require("firebase/app");
 require("firebase/firestore");
 
 const firebaseConfig = {
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  databaseURL: process.env.databaseURL,
+  apiKey: process.env.REACT_APP_APIKey,
+  authDomain: "swe-utd-portal.firebaseapp.com",
+  databaseURL: "https://swe-utd-portal.firebaseio.com",
   projectId: "swe-utd-portal",
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId,
-  measurementId: process.env.measurementId,
+  storageBucket: "swe-utd-portal.appspot.com",
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId,
 };
 
 firebase.initializeApp(firebaseConfig);
