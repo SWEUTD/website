64da04f9a9eb0880200878d3b73f3fb6d5d283b2
Aarushi-Pandey <55065264+Aarushi-Pandey@users.noreply.github.com>
2021-03-01 19:07:04 +0000

    Deploying to gh-pages from  @ 370cedc69370899e970bce7c822f00458a1f23fd 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d4f4bfee7ec4691d3246e8c8fa6ccef1.js"
+  "/precache-manifest.0d7181602acbbcf0f80d0ce859c735e3.js"
 );
 
 self.addEventListener('message', (event) => {
