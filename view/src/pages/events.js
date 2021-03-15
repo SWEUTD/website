9a0ea6140ad5130691fa241851139d94b0faae39
Aarushi Pandey <pandey.aarushi2010@gmail.com>
2021-03-15 14:14:15 -0500

    added new events and flyer, made sign in forms for those events

--- view/src/pages/events.js
+++ view/src/pages/events.js
@@ -18,33 +18,42 @@ import {
 } from "@material-ui/core";
 import NavBar from "../components/navbar";
 
-import Career_Fair_1 from "../assets/EventFlyers/Career_Fair_1.png"
-import Career_Fair_2 from "../assets/EventFlyers/Career_Fair_2.png"
+import Frisco_HS_Panel from "../assets/EventFlyers/Frisco_HS_Panel.png"
 
 const upcomingEvents = [
   {
-    name: "Navigating a Virtual Career Fair Panel",
-    date: '3/10/2021 - 6:00 PM CST',
-    description: "Need some tips on navigating a virtual career fair? Come get advice from our SWE Panelists on internship experiences, networking, and navigating the upcoming virtual career fair! You don't want to miss this event!",
-    link: 'https://nam02.safelinks.protection.outlook.com/?url=https%3A%2F%2Ffacebook.us19.list-manage.com%2Ftrack%2Fclick%3Fu%3Dada9150cb4cf2450b2870f9b7%26id%3D936c50b499%26e%3D09a746bd56&data=04%7C01%7CAarushi.Pandey%40UTDallas.edu%7Cd8ff6a9a48ae4868868208d8dcd846d8%7C8d281d1d9c4d4bf7b16e032d15de9f6c%7C0%7C0%7C637502168750943673%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C1000&sdata=UUt0OL%2BOB9ggysm1e0h8P9mQ4dL%2Bl98of1%2FvSUkD1eM%3D&reserved=0'
+    name: "Get to Know TetraPak!",
+    date: '3/24/2021 - 6:00 PM CST',
+    description: 'Meet Tetra Pak, a world leading food processing and packaging solutions company. They have positions open in a variety of engineering disciplines, and you can learn more at this informational session! Message from TetraPak: "We are offering career opportunities for seniors and recent graduates (1-2 years out of college) who don’t have a lot of experience. It’s a training program called Future Talent. This is a really good program for people who are interested in both technology and sales."',
+    link: 'https://nam02.safelinks.protection.outlook.com/?url=https%3A%2F%2Ffacebook.us19.list-manage.com%2Ftrack%2Fclick%3Fu%3Dada9150cb4cf2450b2870f9b7%26id%3D3acf2d7ff0%26e%3D09a746bd56&data=04%7C01%7CAarushi.Pandey%40UTDallas.edu%7Cc6543fde065b42e298fa08d8e7d5057a%7C8d281d1d9c4d4bf7b16e032d15de9f6c%7C0%7C0%7C637514249399669096%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C1000&sdata=tzFZT7nqxWlAVO03jt3xre5pu6S1wa9WYQWpOOrcA84%3D&reserved=0'
   },
   {
-    name: "Pi Day Social",
-    date: '3/14/2021 - 5:00 PM CST',
-    description: "Come join us for an exciting social to celebrate Pi day with your fellow SWEsters! We have an icebreaker and some fun activities planned - you definitely don't want to miss out!",
-    link: 'https://nam02.safelinks.protection.outlook.com/?url=https%3A%2F%2Ffacebook.us19.list-manage.com%2Ftrack%2Fclick%3Fu%3Dada9150cb4cf2450b2870f9b7%26id%3D422764decf%26e%3D09a746bd56&data=04%7C01%7CAarushi.Pandey%40UTDallas.edu%7Cd8ff6a9a48ae4868868208d8dcd846d8%7C8d281d1d9c4d4bf7b16e032d15de9f6c%7C0%7C0%7C637502168750943673%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C1000&sdata=IXqAVHDK0EmSRs2BG1AKqOuRnz9a94T%2BA3ACI0XHWoE%3D&reserved=0'
+    name: "High School Panel with CTE SWE Next",
+    date: '3/25/2021 - 5:30 PM CST',
+    description: "High school panel with CTE SWE Next where current SWE members will be sharing their experience on what it's like to be a member of SWE in college, provide insight about the several broad engineering fields/topics, as well as any personal experiences in these fields. If you're interested in volunteering as a panelist for our high-school panel events, please watch for future announcements on our Discord!",
+    link: ''
+  },
+  {
+    name: "Fulfilling Your Potential with Intel",
+    date: '3/26/2021 - 4:00 PM CST',
+    description: "Join us for a special event with Intel to learn about goal planning and preparing for next recruiting season, standing out at your internship, and summer to do’s to grow your skills! Don't miss this great opportunity to meet Intel representatives including Lisa Depew!",
+    link: 'https://nam02.safelinks.protection.outlook.com/?url=https%3A%2F%2Ffacebook.us19.list-manage.com%2Ftrack%2Fclick%3Fu%3Dada9150cb4cf2450b2870f9b7%26id%3Dbe91c2b9c1%26e%3D09a746bd56&data=04%7C01%7CAarushi.Pandey%40UTDallas.edu%7Cc6543fde065b42e298fa08d8e7d5057a%7C8d281d1d9c4d4bf7b16e032d15de9f6c%7C0%7C0%7C637514249399689085%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C1000&sdata=CUrnL%2FgAy9Fw0GOwNpffmcLcM10zfuze2vklC2Q%2FMGI%3D&reserved=0'
   },
 ];
 
 const flyers = [
   {
-    image: Career_Fair_1, Career_Fair_2,
+    image: '',
+    link: ''
+  },
+  {
+    image: Frisco_HS_Panel,
     link: ''
   },
   {
     image: '',
     link: ''
-  }
+  },
 ];
 
 
@@ -78,6 +87,7 @@ function createData(event, flyer) {
 const rows = [
   createData(upcomingEvents[0], flyers[0].image),
   createData(upcomingEvents[1], flyers[1].image),
+  createData(upcomingEvents[2], flyers[2].image),
   ];
 
 class events extends Component {
