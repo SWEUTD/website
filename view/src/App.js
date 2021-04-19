ac34045819c0d7eb31a4b5c8ce79f86b2f051087
Aarushi Pandey <pandey.aarushi2010@gmail.com>
2021-04-19 15:40:29 -0500

    updated events page, added 2 flyers, made sign in forms

--- view/src/App.js
+++ view/src/App.js
@@ -66,10 +66,10 @@ function App() {
           <Route exact path="/join" component={join} />
           <Route exact path="/contact" component={contact} />
           <Route exact path="/calendar" component={calendar} />    
-          <PropsRoute exact path="/seniorspanel" eventHeading="Sign in to 'Seniors Panel - If I were a Freshman' Event" eventPoints={1} eventName = "Seniors Panel - If I were a Freshman" eventDate = "4/13/2021" secretWord = "reflection" component={meetingform}/>
-          <PropsRoute exact path="/wwcjp" eventHeading="Sign in to 'WWC X JP Morgan - Imposter Syndrome and Preparing for a Virtual Internship' Event" eventPoints={1} eventName = "WWC X JP Morgan - Imposter Syndrome and Preparing for a Virtual Internship" eventDate = "4/15/2021" secretWord = "advice" component={meetingform}/>
           <PropsRoute exact path="/bridgeland" eventHeading="Sign in to 'Bridgeland High School Panel' Event" eventPoints={1} eventName = "Bridgeland High School Panel" eventDate = "4/20/2021" secretWord = "SWE" component={meetingform}/>
           <PropsRoute exact path="/lennox" eventHeading="Sign in to 'Optimize your Summer with Lennox' Event" eventPoints={1} eventName = "Optimize your Summer with Lennox" eventDate = "4/21/2021" secretWord = "improvement" component={meetingform}/>
+          <PropsRoute exact path="/banquet" eventHeading="Sign in to 'SWE UTDallas Banquet' Event" eventPoints={1} eventName = "SWE UTDallas Banquet" eventDate = "4/23/2021" secretWord = "first" component={meetingform}/>
+          <PropsRoute exact path="/social" eventHeading="Sign in to 'TAMU x UTD SWE End-of-the-Year Social' Event" eventPoints={1} eventName = "TAMU x UTD SWE End-of-the-Year Social" eventDate = "4/28/2021" secretWord = "meet" component={meetingform}/>
         </Switch>
       </BrowserRouter>
     </MuiThemeProvider>
