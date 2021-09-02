b01539187f1065238718d3eeba805db919bd2722
koisaai <43097926+koisaai@users.noreply.github.com>
2021-09-02 08:06:17 +0000

    Deploying to gh-pages from  @ 081d22198d09afe9413e8e0e9200181ff72d0e34 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.924992856ab7bfefcbe77cde7ee4e4b9.js"
+  "/precache-manifest.19c01d7cb878091368d23822880082db.js"
 );
 
 self.addEventListener('message', (event) => {
